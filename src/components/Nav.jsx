da3d8c120a814ed6805b6ef51df06322782e0091
jpreslov <jplov@protonmail.com>
2023-11-04 17:02:25 -0500

    first

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import gif from '../assets/gif.gif';
+
+const Nav = () => {
+    return (
+        <div>
+            <img src={gif} alt="gif" />
+        </div>
+    )
+}
+
+export default Nav;
